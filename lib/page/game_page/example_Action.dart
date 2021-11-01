13374ff80af7805c54136703b83c8da0bf165cd3
tham <thampk@outlook.com>
2021-11-01 18:51:32 +0700

    [ADD]file example_Action

---
+++ lib/page/game_page/example_Action.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class exampleAction extends StatefulWidget {
+  exampleAction({Key? key}) : super(key: key);
+
+  @override
+  _exampleActionState createState() => _exampleActionState();
+}
+
+class _exampleActionState extends State<exampleAction> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
